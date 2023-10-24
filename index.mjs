308585dd5811d1d878c85354306d44308dc6593f
Naveen Kumar Peddi <peddin5739@uhcl.edu>
2023-10-24 08:25:34 -0500

    HTTP METHODS GET,PUT,POST...

--- index.mjs
+++ index.mjs
@@ -1,7 +1,18 @@
 import express from "express";
 const app = express();
+
+// we can use a get,post,put,patch on the same path
+
 app.get("/", (req, res) => {
   res.send("hello express here!");
+});
+
+app.get("/something", (req, res) => {
+  res.send("Some thing component here!!");
+});
+
+app.post("/something", (req, res) => {
+  res.send(" This is  usually not visible");
 });
 app.listen(3200, () => {
   console.log("server listening and ready to go ");
