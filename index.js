01e1619baf696bfde8a3aa7ab1489a1ecd0276ff
Naveen Kumar Peddi <peddin5739@uhcl.edu>
2023-10-24 12:45:59 -0500

    sending static assets

--- index.js
+++ index.js
@@ -2,20 +2,21 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
-// we can use a get,post,put,patch on the same path
+app.use("/static", express.static(path.resolve(__dirname, "public")));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "index.html"));
+  res.sendFile(path.resolve(__dirname, "public/index.html"));
 });
 
 app.get("/something", (req, res) => {
-  res.send("Some thing component here!!");
+  res.send("Something component here!!");
 });
 
 app.post("/something", (req, res) => {
-  res.send(" This is  usually not visible");
+  res.send("This is usually not visible on POST request");
 });
+
 const port = 3200;
 app.listen(port, () => {
-  console.log(`server listening and ready to go ${port} `);
+  console.log(`Server listening and ready to go at http://localhost:${port}`);
 });
